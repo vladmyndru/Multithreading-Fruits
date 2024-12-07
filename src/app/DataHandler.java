55cfb51d3f3c6a38fac70279e5278a8cd85b4010
vladmyndru <ladvis62@gmail.com>
2024-12-07 21:39:35 +0200

    Initial commit

---
+++ src/app/DataHandler.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataHandler {
+
+    private final String [] fruits = new DataRepository().getData();
+
+    public void getOutput() {
+        synchronized (this) {
+            StringBuilder sb = new StringBuilder();
+            AtomicInteger count = new AtomicInteger();
+
+            for (String fruit : fruits) {
+                sb.append(String.format("(%d) %s ", count.getAndIncrement(), fruit));
+            }
+            System.out.println(Thread.currentThread().getName() + ": " + sb);
+        }
+    }
+}
